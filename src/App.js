96bb79eab00d1255b260c64fd7c414372d6555cd
Hamo06 <hamayak0689@gmail.com>
2022-07-15 00:57:59 +0400

    Garri Potter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import './App.scss';
+import Home from './Home/Home';
+import { userinfo } from './Redux/action';
+import { location } from './Redux/action';
+import Gryffindor from './gryffindor/Gryffindor';
+import Slytherin from './Slytherin/Slytherin';
+import Hufflepuff from './Hufflepuff/Hufflepuff';
+import Ravenclaw from './Ravenclaw/Ravenclaw';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {;
+	const dispatch = useDispatch();
+	useEffect(()=> {
+		dispatch(location(window.document.location.pathname));
+		fetch(`http://hp-api.herokuapp.com/api/characters`)
+		.then(stream =>stream.json())
+		.then(results => dispatch(userinfo(results)))
+	},[ dispatch ]);
+	const local = useSelector((state) => state.localReducer);
+	return (
+		<div className="App">
+			{ (local === '/Home' || local === '/')  && <Home /> }
+			{ local === '/Gryffindor' && <Gryffindor /> }
+			{ local === '/Slytherin' && <Slytherin /> }
+			{ local === '/Hufflepuff' && <Hufflepuff /> }
+			{ local === '/Ravenclaw' && <Ravenclaw /> }
+		</div>
+	);
 }
 
 export default App;
